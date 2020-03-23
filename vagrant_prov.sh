8f3a0f949e3fc50e04ce6f54659d2a75699c64c2
Dan Green <danngreen1@gmail.com>
2020-03-23 14:05:05 -0700

    Added vagrantfile to ensure a consistant build environment. Note: does not yet work with cp BUILD/VM-Z.bin /Volumes/DIS_F429ZI/

---
+++ vagrant_prov.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y gcc-arm-none-eabi
+#sudo apt-get install -y build-essential autotools-dev autoconf pkg-config libusb-1.0-0 libusb-1.0-0-dev libftdi1 libftdi-dev git libc6:i386 libncurses5:i386 libstdc++6:i386 cowsay figlet language-pack-en
+
